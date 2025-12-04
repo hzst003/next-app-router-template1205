1e83035fe86d6e950cb245def20db675d5a7d18b
xu <you@example.com>
2025-12-04 01:27:56 -0500

    3

--- src/app/table1203/TableTwoA.tsx
+++ src/app/table1203/TableTwoA.tsx
@@ -1,4 +1,5 @@
-
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useState } from "react";
 
 export default function TableTwoA() {
